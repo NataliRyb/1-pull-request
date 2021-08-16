69a9e0b4fe5b8bc69700421b75e8dd0439255608
NataliRyb <87282374+NataliRyb@users.noreply.github.com>
2021-08-17 00:10:52 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import {Picture} from "./components/Picture";
+import './style.scc';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h4 label/>
+    </div>
+    )
+};
+
+      
+
---
+++ components/Picture/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Picture = ({label}) => {
+    return (
+        <div className='App'>
+            <h4 className="product">Build Your Digital Product That Suitable For Your Need</h4>;
+      </div>
+        
+    );
+       
+};
